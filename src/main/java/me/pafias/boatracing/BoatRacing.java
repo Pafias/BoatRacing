beaf18af52d3956c01bac331ac281dfaee3d5d7c
Pafias <tiagana.b@gmail.com>
2020-12-17 15:39:03 +0100

    Initial commit

---
+++ src/main/java/me/pafias/boatracing/BoatRacing.java
@@ -0,0 +1,17 @@
+package me.pafias.boatracing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BoatRacing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
